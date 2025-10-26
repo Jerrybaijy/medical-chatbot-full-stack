d5ccc924bc3819fc2b61db6ad29a4a7d1001a2b2
jerrybaijy <jerry.baijy@outlook.com>
2025-10-26 20:36:10 +0800

    1st commit

---
+++ template.sh
@@ -0,0 +1,17 @@
+# 样板文件脚本
+
+# 创建目录
+mkdir -p src
+mkdir -p research
+
+# 创建文件
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "样板文件已创建"
